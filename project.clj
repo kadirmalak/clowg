f5f5722fc632f250bdbaacf6f2f111cf79897af2
Kadir Malak <kadir@tazi.ai>
2020-05-21 18:45:35 +0300

    fixed dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
-(defproject clowg "0.1.3"
+(defproject clowg "0.1.4"
   :description "A Clojure library for generating Clojure wrappers around Java"
   :url "https://github.com/kadirmalak/clowg"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.processing/core "3.3.7"]
-                 [org.jogamp.gluegen/gluegen-rt-main "2.3.2"]
-                 [org.jogamp.jogl/jogl-all-main "2.3.2"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns clowg.core})
